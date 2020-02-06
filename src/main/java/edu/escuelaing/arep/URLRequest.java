54b637e06714bacf64a820d0428cfeeeec2978c4
unknown <eduardo.ocampo@labinfo.is.escuelaing.edu.co>
2020-02-06 16:04:28 -0500

    adelanto

--- src/main/java/edu/escuelaing/arep/URLRequest.java
+++ src/main/java/edu/escuelaing/arep/URLRequest.java
@@ -1,13 +1,17 @@
 package edu.escuelaing.arep;
 
+import jdk.nashorn.api.scripting.URLReader;
+
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class URLRequest {
 
-    public static void main(String[] args) throws MalformedURLException {
-        URL page = new URL("http://campusvirtual.escuelaing.edu.co/moodle/mod/assign/view.php?id=34731");
-
+    public void read(URL page){
         System.out.println("Protocol: " + page.getProtocol());
         System.out.println("Authority: " + page.getAuthority());
         System.out.println("Host: " + page.getHost());
@@ -16,7 +20,34 @@ public class URLRequest {
         System.out.println("Query: " + page.getQuery());
         System.out.println("File: " + page.getFile());
         System.out.println("Ref: " + page.getRef());
+    }
+
+    public  void readAndSaveURLPage(URL url) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))){
+            String inputLine = null;
+            File file = new File("src\\main\\java\\edu\\escuelaing\\arep\\resources\\results.html");
+            FileWriter fileWriter = new FileWriter(file);
+            while((inputLine = reader.readLine()) != null){
+                System.out.println(inputLine);
+                fileWriter.write(inputLine);
+            }
+            fileWriter.close();
+        } catch (IOException e){
+            Logger.getLogger(URLReader.class.getName()).log(Level.SEVERE, null, e);
+            System.err.println(e);
+        }
+    }
 
 
+    public static void main(String[] args) throws MalformedURLException {
+        URLRequest reader = new URLRequest();
+        System.out.println("Enter URL, for example: https://www.escuelaing.edu.co/es/ ");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        URL url = new URL(line);
+        System.out.println("Ejercicio 1: ");
+        reader.read(url);
+        System.out.println("Ejercicio 2: ");
+        reader.readAndSaveURLPage(url);
     }
 }
